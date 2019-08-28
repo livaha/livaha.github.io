0d79694658708a6cc65eb8346266c4a43744946e
livaha； <1192137406@qq.com>
2019-08-28 18:04:41 +0800

    test ie

---
+++ precache-manifest.ac3a49a20c6e313b1b6e32555d6fe453.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3dcfa7afd8b12cadc626d2c62ec206b",
+    "url": "/static/media/rep-wisp.f3dcfa7a.png"
+  },
+  {
+    "revision": "036e43e786a4ddd11216",
+    "url": "/static/css/main.82fcaa79.chunk.css"
+  },
+  {
+    "revision": "46bbe7dd75a1a8a18eb5",
+    "url": "/static/js/runtime~main.416a1c7a.js"
+  },
+  {
+    "revision": "b1d1b951b5df3bddc58d",
+    "url": "/static/js/2.37dfadef.chunk.js"
+  },
+  {
+    "revision": "dc069fea1f9abb061085fbf2e24aa63b",
+    "url": "/static/media/logo.dc069fea.png"
+  },
+  {
+    "revision": "1012e857fc369c9df0c13838a15444f6",
+    "url": "/static/media/ap.1012e857.png"
+  },
+  {
+    "revision": "036e43e786a4ddd11216",
+    "url": "/static/js/main.a8f8b917.chunk.js"
+  },
+  {
+    "revision": "06eab7f11f5ac8e5f5aab9e1ef392ab8",
+    "url": "/static/media/router.06eab7f1.png"
+  },
+  {
+    "revision": "0f8648bb2211e07517c44540bc559724",
+    "url": "/static/media/iconfont.0f8648bb.eot"
+  },
+  {
+    "revision": "24fa69146217b160c4d831b9edca01ea",
+    "url": "/static/media/iconfont.24fa6914.ttf"
+  },
+  {
+    "revision": "fe723b2d6ff424065197106ee57dc6b1",
+    "url": "/static/media/iconfont.fe723b2d.woff"
+  },
+  {
+    "revision": "7bcd46a2c7c675828fe59c737035754c",
+    "url": "/static/media/iconfont.7bcd46a2.svg"
+  },
+  {
+    "revision": "a58af6c8bec1329348e777b14fdd7b40",
+    "url": "/index.html"
+  }
+];
