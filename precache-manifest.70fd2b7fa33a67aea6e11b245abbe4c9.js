36008f6593af7b742c8f543f767fa76a0ef496c1
livaha； <1192137406@qq.com>
2019-09-04 10:26:03 +0800

    test react16.9 IE 11, ie9

---
+++ precache-manifest.70fd2b7fa33a67aea6e11b245abbe4c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c84f9953c5266f87c4faea73040db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b5670354609de9b166",
+    "url": "/static/css/2.5130aed7.chunk.css"
+  },
+  {
+    "revision": "de55eebf84beff450f27",
+    "url": "/static/css/main.89b7b774.chunk.css"
+  },
+  {
+    "revision": "57b5670354609de9b166",
+    "url": "/static/js/2.3c98fac7.chunk.js"
+  },
+  {
+    "revision": "de55eebf84beff450f27",
+    "url": "/static/js/main.c563b93b.chunk.js"
+  },
+  {
+    "revision": "67b18c462aad0a5c1906",
+    "url": "/static/js/runtime~main.53b5d61a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
