a45e18b1db1a28b439b522ade7d4136dd0c1b24e
livaha <1192137406@qq.com>
2019-09-08 01:06:42 +0800

    rm map,rm image

---
+++ precache-manifest.a7499220c0f70176b29960facdbea7b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515cb9982701779d1946eab869ab8997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f095c6073a3db954ed11",
+    "url": "/static/css/2.41e35f1d.chunk.css"
+  },
+  {
+    "revision": "ed53db6bc84d1614771d",
+    "url": "/static/css/main.790d2e51.chunk.css"
+  },
+  {
+    "revision": "f095c6073a3db954ed11",
+    "url": "/static/js/2.8329398d.chunk.js"
+  },
+  {
+    "revision": "ed53db6bc84d1614771d",
+    "url": "/static/js/main.d614b3ec.chunk.js"
+  },
+  {
+    "revision": "67b18c462aad0a5c1906",
+    "url": "/static/js/runtime~main.53b5d61a.js"
+  },
+  {
+    "revision": "cde88ea61f511b1c6bbc8fbb3623c31e",
+    "url": "/static/media/logo.cde88ea6.png"
+  }
+]);
