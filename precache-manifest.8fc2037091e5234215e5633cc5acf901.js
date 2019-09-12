6ddabd81c7fe86d01468d3c21074c71d2ad670eb
livaha； <1192137406@qq.com>
2019-09-12 11:51:04 +0800

    new pages

---
+++ precache-manifest.8fc2037091e5234215e5633cc5acf901.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cde88ea61f511b1c6bbc8fbb3623c31e",
+    "url": "/static/media/logo.cde88ea6.png"
+  },
+  {
+    "revision": "ee50242b2d4b3e7635cc2c90b24788d1",
+    "url": "/static/media/emui-icon.ee50242b.png"
+  },
+  {
+    "revision": "44fe01edfb9157f7bff5",
+    "url": "/static/js/runtime~main.51def588.js"
+  },
+  {
+    "revision": "ae5d789fed7c0983c2ec",
+    "url": "/static/js/main.a0aa6cc0.chunk.js"
+  },
+  {
+    "revision": "6e386cc19496e73b7cb3",
+    "url": "/static/js/2.fec3e390.chunk.js"
+  },
+  {
+    "revision": "ae5d789fed7c0983c2ec",
+    "url": "/static/css/main.6d81db5d.chunk.css"
+  },
+  {
+    "revision": "6e386cc19496e73b7cb3",
+    "url": "/static/css/2.97dd6289.chunk.css"
+  },
+  {
+    "revision": "4c68d8575ef4af8e594fd9f2f9a3e666",
+    "url": "/index.html"
+  }
+];
